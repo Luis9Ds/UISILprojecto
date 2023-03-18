46ab4d94da924e8cb0d43a8bf2b6dbaa3de34299
Luis9Ds <diegopicadobe@gmail.com>
2023-03-18 03:56:58 -0600

    practica_de_clase_2

--- practica_de_clase_2.py
+++ practica_de_clase_2.py
@@ -3,6 +3,8 @@
 Usuario = input(f"Su nombre de usuario es: ")
 print(f"Bienvenido {Usuario} ")
 
+
+
 # Parte 2 "Con razon se llamaba "de investigacion" fue algo tardado encontrar que existian los .lower .upper"
 print("------------------------------")
 nombre_completo = input("Porfavor escriba su nombre completo: ")
@@ -13,3 +15,10 @@ print(nombre_completo.upper())
 
 print(nombre_completo.title())
 
+# Parte 3 Esto fue una tortura de sacar, especificamente porque todo el rato me sacaba de resultado el nombre tipo "LuisLuisLuis" En vez de un salto de linea de codigo
+print("------------------------------")
+Administrador = input(f"Bienvendido: ")
+Repeticion = int(input("Por favor ingresa un numero: "))
+
+
+print("me gusta tu nombre \n" + (Administrador + "\n") * Repeticion)
