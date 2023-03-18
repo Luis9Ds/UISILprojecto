fe9de9e37f3df2454eb52fdddf3dd767bf4b3995
Luis9Ds <diegopicadobe@gmail.com>
2023-03-18 03:32:43 -0600

    parte 2

---
+++ practica_de_clase_2.py
@@ -0,0 +1,15 @@
+# Esto va a ser parte de la investagacion sobre input 
+
+Usuario = input(f"Su nombre de usuario es: ")
+print(f"Bienvenido {Usuario} ")
+
+# Parte 2 "Con razon se llamaba "de investigacion" fue algo tardado encontrar que existian los .lower .upper"
+print("------------------------------")
+nombre_completo = input("Porfavor escriba su nombre completo: ")
+
+print(nombre_completo.lower())
+
+print(nombre_completo.upper())
+
+print(nombre_completo.title())
+
