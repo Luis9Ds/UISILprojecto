bae098cb5ad4a508583c1d1ecbbf6686648cd316
Luis9Ds <diegopicadobe@gmail.com>
2023-03-17 19:53:44 -0600

    avanzes

--- practica_de_clase.py
+++ practica_de_clase.py
@@ -1,12 +1,28 @@
 # Esta pagina es para las 2 primeras practicas correspondientes a la clase de Logica y Algoritmos
 
 # 1:
+print("-------------------------------------------------------------------------------------------------------")
 print("Hola Mundo")
 
 # 2:
+print("-------------------------------------------------------------------------------------------------------")
 Saludo = "Hola Mundo"
 print(Saludo)
 
 # 3:
+print("-------------------------------------------------------------------------------------------------------")
 Usuario = "Luis Diego"
 print(f"Hola {Usuario}")
+
+# 4:
+print("-------------------------------------------------------------------------------------------------------")
+print("Profe cual operacion???")
+
+# 5:
+print("-------------------------------------------------------------------------------------------------------")
+Peso = 78
+Estatura = 1.76
+Estatura_al_cuadrado = Estatura * Estatura
+IMC = Peso / Estatura_al_cuadrado
+
+print(f"Su indice de masa corporal corresponde a {IMC}")
