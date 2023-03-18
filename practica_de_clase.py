5e30b4a2c1d64a277015376c0cea3f56df5df7a3
Luis9Ds <diegopicadobe@gmail.com>
2023-03-17 19:37:30 -0600

    Guardado de emergencia

---
+++ practica_de_clase.py
@@ -0,0 +1,12 @@
+# Esta pagina es para las 2 primeras practicas correspondientes a la clase de Logica y Algoritmos
+
+# 1:
+print("Hola Mundo")
+
+# 2:
+Saludo = "Hola Mundo"
+print(Saludo)
+
+# 3:
+Usuario = "Luis Diego"
+print(f"Hola {Usuario}")
