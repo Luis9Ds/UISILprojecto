a44099fa20a7f202c4906810afd046f12ab84ab9
Luis9Ds <diegopicadobe@gmail.com>
2023-03-19 22:32:34 -0600

    Final

--- practica_de_clase.py
+++ practica_de_clase.py
@@ -19,8 +19,19 @@ print("-------------------------------------------------------------------------
 resultado = ((3 + 2) / (2 * 5)) ** 2
 print(f" el resultado de la operacion es {resultado}")
 
+# 5: Cosa curiosa, no habia visto este ejercicio y lo hice como 4 dias despues de que terminase todo jajaja
+print("-------------------------------------------------------------------------------------------------------")
+Horas_trabajadas = 8
+coste_por_hora = 15000
+Pago = Horas_trabajadas * coste_por_hora
 
-# 5: Despues de hacer la practica del Input, me devolvi a esta parte para hacer tal cual lo que pedia la practica
+print("¿Cuantas horas ah estado laburando?")
+print(Horas_trabajadas)
+
+print(f"Su paga corresponde a {Pago} Colones")
+
+
+# 6: Despues de hacer la practica del Input, me devolvi a esta parte para hacer tal cual lo que pedia la practica
 print("-------------------------------------------------------------------------------------------------------")
 peso = int(input("Por favor ingrese su peso en kg: "))
 estatura = float(input("Por favor ingrese su estatura en metros: "))
@@ -30,7 +41,7 @@ imc = peso / estatura_al_cuadrado
 print(f"Tu índice de masa corporal es {round(imc, 2)}")
 
 
-# 6:
+# 7:
 print("-------------------------------------------------------------------------------------------------------")
 Numero_1 = 40
 Numero__2 = 20
